38f6c61b30cbf3587d001d24ff9da58edff79c68
Ed Posnak <ejp@eigence.com>
2019-08-15 13:53:13 +0200

    use exact trade sizes, compare price (not rate)

--- autorun.sh
+++ autorun.sh
@@ -1,10 +1,10 @@
 while true; do
     echo "running ..."
     python3 totle_api_client_v2.py
-    sleep 1200
+    sleep 300
     python3 totle_api_client_v2.py --sell
     echo "sleeping ..."
-    sleep 1200
+    sleep 300
 done
 
     
