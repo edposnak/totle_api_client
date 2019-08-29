48df827d1cf9dec4d48bc1279fb402b3edc95feb
Ed Posnak <ejp@eigence.com>
2019-08-29 20:24:09 +0200

    allow arguments and clean termination using stop file

--- autorun.sh
+++ autorun.sh
@@ -1,12 +1,12 @@
-while true; do
+FILE='autorun.stop'
+
+while [ ! -f "$FILE" ]; do
     echo "$(date "+%Y-%m-%d-%H:%M:%S") running ..."
-    python3 totle_api_client_v2.py
-    echo "$(date "+%Y-%m-%d-%H:%M:%S") sleeping ..."
-    sleep 60
-    echo "$(date "+%Y-%m-%d-%H:%M:%S") running ..."
-    python3 totle_api_client_v2.py --sell
+    python3 totle_api_client_v2.py $*
     echo "$(date "+%Y-%m-%d-%H:%M:%S") sleeping ..."
     sleep 60
 done
 
+echo "terminating and removing $FILE"
+rm $FILE
     
