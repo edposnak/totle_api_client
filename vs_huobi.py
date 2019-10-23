a250d09d8307629b652a62441bdaf5325acfbe33
Ed Posnak <ejp@eigence.com>
2019-10-22 21:19:40 -0700

    move fee to huobi_client

--- vs_huobi.py
+++ vs_huobi.py
@@ -1,18 +1,14 @@
 import v2_client
-from v2_compare_prices import get_savings, print_savings
+from v2_compare_prices import get_cex_savings, print_savings
 import huobi_client
 
 #####################################################################
-
-TAKER_FEE_PCT = 0.2
-# 0.2% for lowest tier (sources: https://www.huobi.co/en-us/fee https://huobiglobal.zendesk.com/hc/en-us/articles/360000210281-Announcement-New-Tiered-Fee-Structure)
-# 0.03% for VIPs (DMs?) (sources: https://www.huobi.co/en-us/fee https://huobiglobal.zendesk.com/hc/en-us/articles/360000113122-Fees)
 
 TRADE_SIZES = [0.1, 1.0, 5.0, 10.0]
 quote='ETH'
 overlap_pairs = [ (b,q) for b,q in huobi_client.get_pairs() if q == quote and b in v2_client.tokens ]
 
 for order_type in ['buy', 'sell']:
-    savings = get_savings(huobi_client, order_type, overlap_pairs, TRADE_SIZES, TAKER_FEE_PCT)
+    savings = get_cex_savings(huobi_client, order_type, overlap_pairs, TRADE_SIZES)
     print_savings(order_type, savings, TRADE_SIZES)
 
