817b5d710105cee4d9f8e4cc72a4d83690cda9de
Ed Posnak <ejp@eigence.com>
2019-10-31 09:56:30 -0700

    use multiple sources of dex/token info

--- summarize_order_splitting_data.py
+++ summarize_order_splitting_data.py
@@ -9,7 +9,7 @@ import dexag_client
 import oneinch_client
 import paraswap_client
 import v2_client
-
+import exchange_utils
 
 DEX_AG = dexag_client.name()
 ONE_INCH = oneinch_client.name()
@@ -20,20 +20,43 @@ AGG_NAMES = [DEX_AG, ONE_INCH, PARASWAP]
 
 COMMON_DEXS = ['0xMesh', 'AirSwap', 'Bancor', 'Kyber', 'Oasis', 'Radar Relay', 'Uniswap']
 
-# def print_tokens_dex_matrix(tok_ts_dexs_with_pair, dexs=COMMON_DEXS):
-#     dex_strs = map(lambda d: f"{d:<12}", dexs)
-#     print(f"{'Token':<12}{''.join(dex_strs)}")
-#     for token, ts_dexs in tok_ts_dexs_with_pair.items():
-#         dexs_for_token = set(sum(ts_dexs.values(), []))
-#         dex_strs = map(lambda d: f"{'  X':<12}" if d in dexs_for_token else f"{' ':<12}", dexs)
-#         print(f"{token:<12}{''.join(dex_strs)}")
+def get_tokens_dexs(tok_ts_dex_prices, totle_tok_ts_dex_prices, tok_ts_dexs_with_pair):
+    """Returns a dict of tokens: dexes found to support the token based on the 3 input dicts"""
+    token_dexs = defaultdict(set)
 
-# def print_tokens_dex_csv(tok_ts_dexs_with_pair, dexs=COMMON_DEXS):
-#     print(f"Token,{','.join(dexs)}")
-#     for token, ts_dexs in tok_ts_dexs_with_pair.items():
-#         dexs_for_token = set(sum(ts_dexs.values(), []))
-#         dex_strs = map(lambda d: 'X' if d in dexs_for_token else '', dexs)
-#         print(f"{token},{','.join(dex_strs)}")
+    # get dexs used by agg's supplying dex prices
+    for token, ts_agg_dex_prices in tok_ts_dex_prices.items():
+        dexs_for_token = set()
+        for ts, agg_dex_prices in ts_agg_dex_prices.items():
+            for agg, dex_prices in agg_dex_prices.items():
+                for prices in dex_prices:
+                    dexs_for_token |= prices.keys()
+        token_dexs[token] |= dexs_for_token
+
+    # since 1-Inch doesn't provide any dex prices we use tok_ts_dexs_with_pair, to take into account 1-Inch's splits
+    for token, ts_dexs in tok_ts_dexs_with_pair.items():
+        token_dexs[token] |= set(sum(ts_dexs.values(), []))
+
+    # add in Totle's dex prices
+    for token, ts_agg_dex_prices in totle_tok_ts_dex_prices.items():
+        dexs_for_token = set()
+        for ts, agg_dex_prices in ts_agg_dex_prices.items():
+            for agg, dex_prices in agg_dex_prices.items():
+                for prices in dex_prices:
+                    dexs_for_token |= prices.keys()
+        token_dexs[token] |= dexs_for_token
+
+    # sort the keys and canonize/uniq the values
+    return { token: list(set(exchange_utils.canonical_names(dexs))) for token, dexs in sorted(token_dexs.items()) }
+
+
+
+def print_tokens_dex_csv(token_dexs, dex_columns):
+    print(f"Token,{','.join(dex_columns)}")
+    for token, dexs in token_dexs.items():
+        dex_strs = map(lambda d: 'X' if d in dexs else '', dex_columns)
+        print(f"{token},{','.join(dex_strs)}")
+
 
 def min_splits(tok_ts_splits_by_agg, verbose=False):
     """Returns dict of split thresholds for various tokens by each DEX aggregator, i.e. token: { agg: min_trade_size }"""
@@ -188,66 +211,78 @@ totle_tok_ts_dex_prices = get_all_dex_prices(glob.glob(f'{DATA_DIR}/totle*ts_dex
 
 all_dexs, all_tokens = all_dexs_and_tokens(tok_ts_dexs_with_pair, totle_tok_ts_dexs_with_pair)
 
-def get_tokens_dexs(tok_ts_dex_prices, totle_tok_ts_dex_prices, tok_ts_dexs_with_pair):
-    token_dexs = defaultdict(set)
+totle_tokens = sorted(totle_tok_ts_dexs_with_pair.keys())  # subset of agg_tokens
+print(f"len(totle_tokens)={len(totle_tokens)}")
 
-    # get dexs used by agg's supplying dex prices
-    for token, ts_agg_dex_prices in tok_ts_dex_prices.items():
-        dexs_for_token = set()
-        for ts, agg_dex_prices in ts_agg_dex_prices.items():
-            for agg, dex_prices in agg_dex_prices.items():
-                for prices in dex_prices:
-                    dexs_for_token |= prices.keys()
-        token_dexs[token] |= dexs_for_token
-
-    # since 1-Inch doesn't provide any dex prices we use tok_ts_dexs_with_pair, to take into account 1-Inch's splits
-    for token, ts_dexs in tok_ts_dexs_with_pair.items():
-        token_dexs[token] |= set(sum(ts_dexs.values(), []))
-
-    # add in Totle's dex prices
-    for token, ts_agg_dex_prices in totle_tok_ts_dex_prices.items():
-        dexs_for_token = set()
-        for ts, agg_dex_prices in ts_agg_dex_prices.items():
-            for agg, dex_prices in agg_dex_prices.items():
-                for prices in dex_prices:
-                    dexs_for_token |= prices.keys()
-        token_dexs[token] |= dexs_for_token
-
-    return { token : list(dexs) for token, dexs in token_dexs.items() }
-
-
-def print_tokens_dex_csv(token_dexs, dex_columns):
-    print(f"Token,{','.join(dex_columns)}")
-    for token, dexs in token_dexs.items():
-        dex_strs = map(lambda d: 'X' if d in dexs else '', dex_columns)
-        print(f"{token},{','.join(dex_strs)}")
-
-
+# Print dex/token matrix in CSV form
 token_dexs = get_tokens_dexs(tok_ts_dex_prices, totle_tok_ts_dex_prices, tok_ts_dexs_with_pair)
-print_tokens_dex_csv(token_dexs, all_dexs)
+# print_tokens_dex_csv(token_dexs, all_dexs)
 
-# TODO
-#   3. print a list of tokens supported by each exchange
-
-exit(0)
-
+# print a list of tokens supported by each exchange
+for dex in all_dexs:
+    supported_tokens = [t for t in token_dexs if dex in token_dexs[t]]
+    print(f"{dex},{len(supported_tokens)},\"{','.join(supported_tokens)}\"" )
 
 print(f"{len(all_tokens)} tokens: ", ', '.join(all_tokens))
 print(f"{len(all_dexs)} DEXs: ", ', '.join(all_dexs))
 
-print(f"\n\nTokens available by number of DEXs tradable: ")
+print(f"\n\nTokens by number of DEXs listing the ETH pair: ({len(tok_ts_dexs_with_pair)} tokens)")
 # TODO use results combining prices and totle's data
+
+# get_tokens_dexs(tok_ts_dex_prices, totle_tok_ts_dex_prices, tok_ts_dexs_with_pair)
 for i in range(len(all_dexs)+1):
-    tokens_at_i = sorted([ token for token, ts_dexs in tok_ts_dexs_with_pair.items() if len(set(sum(ts_dexs.values(), []))) == i ])
+    tokens_at_i = sorted([ token for token, dexs in token_dexs.items() if len(dexs) == i ])
     print(f"{i} {','.join(tokens_at_i)}")
 
-# print("\n\n")
-# print_tokens_dex_matrix(tok_ts_dexs_with_pair, all_dexs)
-# print_tokens_dex_csv(tok_ts_dexs_with_pair, all_dexs)
 
-print("\n\n")
-print(json.dumps(token_splits(tok_ts_splits_by_agg), indent=3))
+TOTLE_56 = ['ANT','AST','BAT','BMC','BNT','CDAI','CDT','CETH','CND','CUSDC','CVC','CWBTC','CZRX','DAI','DENT','ENG','ENJ','ETHOS','FUN','GNO','KNC','LEND','LINK','MANA','MCO','MKR','MTL','NEXO','NPXS','OMG','PAX','PAY','PLR','POE','POLY','POWR','RCN','RDN','REN','REP','REQ','RLC','RPL','SNT','SNX','SPANK','STORJ','TAU','TKN','TUSD','USDC','USDT','VERI','WBTC','XDCE','ZRX']
+# To compute TOTLE_56
+# totle_56_tokens = set()
+# for token, ts_dexs in totle_tok_ts_dexs_with_pair.items():
+#     for ts, dexs in ts_dexs.items():
+#         if dexs: totle_56_tokens.add(token)
+# print(f"len(totle_56_tokens)={len(totle_56_tokens)}")
+# print(f"TOTLE_56 = [{','.join(map(repr,totle_56_tokens)) }]")
+# These were a tries at comprehensions that don't filter correctly
+# sup_tokens = set([ token for ts, dexs in ts_dexs.items() for token, ts_dexs in totle_tok_ts_dexs_with_pair.items() if dexs ])
+# sup_tokens = set([ token for ts, dexs in ts_dexs.items() if dexs for token, ts_dexs in totle_tok_ts_dexs_with_pair.items()  ])
+# print(f"len(sup_tokens)={len(sup_tokens)}")
+
+ACTIVE_TOTLE_DEXS = ['Ether Delta', 'Kyber', 'Bancor', 'Oasis', 'Uniswap', 'Compound', '0xMesh']
+
+print(f"\n\nTokens supported by Totle by number of DEXs active on Totle: ({len(TOTLE_56)} tokens)")
+ndexs_tokens = defaultdict(list)
+for totle_token in TOTLE_56:
+    ts_dexs = tok_ts_dexs_with_pair[totle_token]
+    dexs = token_dexs[totle_token]
+
+
+    # dexs = exchange_utils.canonical_names(set(sum(ts_dexs.values(), [])))
+    dexs_active_on_totle = [ d for d in dexs if d in ACTIVE_TOTLE_DEXS ]
+    # dexs_active_on_totle = dexs
+    if totle_token.endswith('DAI'): print(f"{totle_token} has {len(dexs_active_on_totle)}/{len(dexs)} DEXs active on Totle dexs={dexs} ")
+    ndexs_tokens[len(dexs_active_on_totle)].append(totle_token)
+for n, tokens in sorted(ndexs_tokens.items()):
+    print(f"{n} {','.join(tokens)}")
+
+sum = 0
+for n, tokens in sorted(ndexs_tokens.items()):
+    sum += len(tokens)
+    print(sum)
 exit(0)
+
+
+print(f"\n\nTokens by number of DEXs listing the ETH pair: ({len(TOTLE_56)} tokens)")
+for i in range(len(all_dexs)+1):
+    tokens_at_i = sorted([ token for token, ts_dexs in tok_ts_dexs_with_pair.items() if len(set(sum(ts_dexs.values(), []))) == i and token in TOTLE_56 ])
+    print(f"{i} {','.join(tokens_at_i)}")
+
+
+exit(0)
+
+
+print("\n\ntoken_splits(tok_ts_splits_by_agg)")
+print(json.dumps(token_splits(tok_ts_splits_by_agg), indent=3))
 
 
 print("\n\nTokens and aggregators providing quotes")
