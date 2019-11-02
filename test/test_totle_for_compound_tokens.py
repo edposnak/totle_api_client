59fa4be987d8a315fccf8637abe5c2834e9a604a
Ed Posnak <ejp@eigence.com>
2019-11-01 19:16:53 -0700

    add test for compound tokens

---
+++ test/test_totle_for_compound_tokens.py
@@ -0,0 +1,53 @@
+import json
+import sys
+from collections import defaultdict
+from datetime import datetime
+import v2_client
+import dexag_client
+import oneinch_client
+import paraswap_client
+
+import exchange_utils
+
+TOTLE_EX = v2_client.name()
+
+QUOTE = 'ETH'
+TRADE_SIZES = [0.1, 0.5, 1.0, 5.0, 10.0, 50.0, 100.0, 200.0, 300.0, 400.0]
+COMPOUND_TOKENS = ['CBAT','CDAI','CETH','CREP','CUSDC','CWBTC','CZRX']
+TOTLE_EXCHANGES = integrated_exchanges = list(v2_client.exchanges().keys())
+
+
+def get_totle_data(tokens=TOKENS, trade_sizes=TRADE_SIZES, quote=QUOTE, exchanges=TOTLE_EXCHANGES):
+    # get list of tokens on dexag and 1-inch that are tradable/splittable
+    tok_ts_dexs_with_pair = defaultdict(dict)
+    tok_ts_splits_by_agg = defaultdict(dict)
+    tok_ts_dex_prices = defaultdict(dict)
+
+    # TODO: sells and compare with buys
+    for base in tokens:
+        for trade_size in trade_sizes:
+            print(f"Doing {base} at {trade_size} {quote} ...")
+            dexs_with_pair, splits_by_agg, dex_prices = set(), {}, {}
+            splits_by_agg[TOTLE_EX] = {} # there will just be this one entry, which will list individual DEXs that returned prices
+
+            for dex in exchanges:
+                can_dex = exchange_utils.canonical_name(dex)
+                if dex == 'Compound' and not base in COMPOUND_TOKENS: continue  # don't waste queries for non-C tokens
+
+                pq = v2_client.get_quote(quote, base, from_amount=trade_size, dex=dex)
+                if not pq:
+                    print(f"{can_dex} did not have {quote} to {base} at trade size={trade_size}")
+                else:
+                    splits_by_agg[TOTLE_EX][can_dex] = -1  # -1 indicates this is not a split, just a list of dexs that could be used
+                    dexs_with_pair.add(can_dex)
+                    dex_prices[can_dex] = pq['price']
+
+            tok_ts_dexs_with_pair[base][trade_size] = list(dexs_with_pair)
+            tok_ts_splits_by_agg[base][trade_size] = splits_by_agg
+            tok_ts_dex_prices[base][trade_size] = dex_prices
+
+    print(json.dumps(tok_ts_dexs_with_pair, indent=3))
+
+########################################################################################################################
+# main
+get_totle_data(tokens=COMPOUND_TOKENS, trade_sizes=[0.2, 2.0], exchanges=['Compound'])
---
+++ totle_vs_aggs.py
@@ -0,0 +1,78 @@
+import json
+from collections import defaultdict
+import concurrent.futures
+
+import dexag_client
+import exchange_utils
+import oneinch_client
+import paraswap_client
+import v2_client
+from v2_compare_prices import savings_data, print_savings, get_filename_base, SavingsCSV
+
+#####################################################################
+
+quote='ETH'
+order_type = 'buy'
+
+TOTLE_39 = ['ANT','AST','BAT','BNT','CDT','CND','CVC','DAI','ENG','ENJ','ETHOS','GNO','KNC','LINK','MANA','MCO','MKR','OMG','PAX','PAY','POE','POLY','POWR','RCN','RDN','REN','REP','REQ','RLC','RPL','SNT','SNX','STORJ','TKN','TUSD','USDC','USDT','WBTC','ZRX']
+AGG_CLIENTS = [dexag_client, oneinch_client, paraswap_client]
+all_buy_savings = defaultdict(lambda: defaultdict(lambda: defaultdict(dict))) # extra lambda prevents KeyError in print_savings
+
+TOKENS, TRADE_SIZES  = TOTLE_39, [0.1, 0.5, 1.0, 5.0, 10.0, 50.0, 100.0, 500.0]
+# TOKENS, TRADE_SIZES = ['CVC', 'DAI', 'LINK'], [0.5, 5.0]
+
+filename = get_filename_base(prefix='totle_vs_aggs', suffix=order_type)
+
+
+def compare_totle_and_aggs(agg_clients, base, quote, trade_size):
+    agg_savings = {}
+    from_token, to_token = quote, base
+    # this hardcodes sourceAmount=trade_size because from_token (AKA quote) is ETH
+    params = {'orderType': order_type, 'tradeSize': trade_size}
+    totle_sd = v2_client.try_swap(v2_client.name(), from_token, to_token, params=params, verbose=False)
+    if totle_sd:
+        totle_price = totle_sd['price']
+        totle_used = totle_sd['totleUsed']
+
+        futures_agg = {}
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            for agg_client in agg_clients:
+                future = executor.submit(agg_client.get_quote, from_token, to_token, from_amount=trade_size)
+                futures_agg[future] = agg_client.name()
+
+        for f in concurrent.futures.as_completed(futures_agg):
+            agg_name = futures_agg[f]
+            pq = f.result()
+            # print(f"{agg_name}: {pq}")
+            if pq:
+                agg_price = pq['price']
+                pct_savings = 100 - (100.0 * totle_price / agg_price)
+                splits = exchange_utils.canonical_keys(pq['exchanges_parts'])
+                savings = savings_data(order_type, trade_size, base, agg_name, pct_savings, totle_used, totle_price, agg_price, splits)
+                agg_savings[agg_name] = savings
+                csv_writer.append(savings)
+
+                trade_info = f"trade size={trade_size} ETH (Totle price={totle_price:.5g} {agg_name} price={agg_price:.5g})"
+                if splits: trade_info += f"splits={splits}"
+                print(f"Totle saved {pct_savings:.2f} percent vs {agg_name} {order_type}ing {base} on {','.join(totle_used)} {trade_info}")
+
+            else:
+                print(f"{agg_name} had no price quote for {order_type} {base} / {trade_size} {quote}")
+    return agg_savings
+
+########################################################################################################################
+# main
+
+with SavingsCSV(filename) as csv_writer:
+    for base in TOKENS:
+        for trade_size in TRADE_SIZES:
+            agg_savings = compare_totle_and_aggs(AGG_CLIENTS, base, quote, trade_size)
+            for agg_name in agg_savings:
+                all_buy_savings[agg_name][base][trade_size] = agg_savings[agg_name]
+
+# print(json.dumps(all_buy_savings, indent=3))
+
+# Prints a savings dict, token => trade_size => savings values
+for agg_name in all_buy_savings:
+    print_savings(order_type, all_buy_savings[agg_name], TRADE_SIZES, title=f"Savings vs. {agg_name}")
+
