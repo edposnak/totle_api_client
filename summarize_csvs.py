7f935ac5553e94afee47660ba0dba9e5de3ba630
Ed Posnak <ejp@eigence.com>
2019-11-01 19:10:47 -0700

    print CSVs

--- summarize_csvs.py
+++ summarize_csvs.py
@@ -20,27 +20,34 @@ per_token_savings = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
 pos_samples, neg_samples = 0, 0
 neg_savings, pos_savings = defaultdict(int), defaultdict(int)
 
+ONLY_SPLITS, ONLY_NON_SPLITS = True, False
+EXCLUDES = [('DEX.AG', 'ETHOS')]
+
 for file in csv_files:
     with open(file, newline='') as csvfile:
         reader = csv.DictReader(csvfile, fieldnames=None)
         for row in reader:
             if row.get('splits'):
                 splits = exchange_utils.canonical_keys(eval(row['splits']))
-                # TODO do something if len(splits) > 1: print(splits)
-
+                if ONLY_SPLITS and len(splits) < 2: continue
+                if ONLY_NON_SPLITS and len(splits) > 1: continue
 
             token = row['token']
-            pts = per_token_savings[token]
-
             trade_size = row['trade_size']
             trade_size_savings = per_trade_size_savings[trade_size]
-            pts_tss = pts[trade_size]
-
             exchange = row['exchange']
             pct_savings = float(row['pct_savings'])
 
+            if (exchange, token) in EXCLUDES:
+                # print(f"Excluding data for {token} on {exchange}")
+                continue
+
+            if exchange == '1-Inch' and token =='ZRX' and trade_size in ['0.1', '0.5'] and pct_savings < -1.0:
+                print(f"Excluding {row}")
+                continue
+
             trade_size_savings[exchange].append(pct_savings)
-            pts_tss[exchange].append(pct_savings)
+            per_token_savings[token][trade_size][exchange].append(pct_savings)
 
             if pct_savings > 0.0:
                 pos_samples += 1
@@ -51,7 +58,7 @@ for file in csv_files:
 
 ############################################################################
 # print neg savings stuff
-exchanges = list(set(neg_savings.keys()) | set(pos_savings.keys()))
+exchanges = sorted(list(set(neg_savings.keys()) | set(pos_savings.keys())))
 
 total_samples = pos_samples + neg_samples
 neg_pct = 100.0 * neg_samples / total_samples
@@ -69,8 +76,6 @@ for exchange in exchanges:
         row.append("")
 
 print("\t".join(row))
-
-
 
 ############################################################################
 # print average savings by trade_size, exchange with num samples
@@ -91,8 +96,10 @@ print_savings_with_num_samples(per_trade_size_savings)
 def print_savings_summary_table(savings_by_trade_size, all_exchanges):
     headers = ''.join(list(map(lambda e: f"{e:<18}", all_exchanges)))
     print(f"{'Trade Size':<18}{headers}")
-    for trade_size, savings in savings_by_trade_size.items():
-        row = f"{trade_size:<4} ETH          "
+    # for trade_size, savings in savings_by_trade_size.items():
+    for trade_size in map(str, sorted(map(float, savings_by_trade_size.keys()))):
+        savings = savings_by_trade_size[trade_size]
+        row = f"{trade_size:<6} ETH        "
         for exchange in all_exchanges:
             if exchange in savings:
                 sum_savings, n_samples = sum(savings[exchange]), len(savings[exchange])
@@ -102,17 +109,37 @@ def print_savings_summary_table(savings_by_trade_size, all_exchanges):
                 row += f"{'-':18}"
         print("".join(row))
 
+def print_savings_summary_table_csv(savings_by_trade_size, all_exchanges):
+    print(f"Trade Size,{','.join(all_exchanges)}")
+    for trade_size in map(str, sorted(map(float, savings_by_trade_size.keys()))):
+        savings = savings_by_trade_size[trade_size]
+        row = f"{trade_size}"
+        for exchange in all_exchanges:
+            if exchange in savings:
+                sum_savings, n_samples = sum(savings[exchange]), len(savings[exchange])
+                pct_savings = sum_savings / n_samples
+                row += f",{pct_savings:.2f}"
+            else:
+                row += f","
+        print(row)
+
 
 print("\n\n")
 print_savings_summary_table(per_trade_size_savings, exchanges)
+print("\n\n")
+print_savings_summary_table_csv(per_trade_size_savings, exchanges)
 
 ############################################################################
 # print average savings by token
 
-def print_per_token_savings_summary_table(per_token_savings):
+def print_per_token_savings_summary_tables(per_token_savings, exchanges):
     for token, tss in per_token_savings.items():
         print(f"\n\n{token}")
         print_savings_summary_table(tss, exchanges)
 
 print("\n\n")
-print_per_token_savings_summary_table(per_token_savings)
+print_per_token_savings_summary_tables(per_token_savings, exchanges)
+
+for token in ['ENJ','BAT','MKR','DAI']:
+    print(f"\n{token}")
+    print_savings_summary_table_csv(per_token_savings[token], exchanges)
