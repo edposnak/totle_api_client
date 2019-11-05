6f9471f28b84c74b0223cac58bae6bd45b7fcf0d
Ed Posnak <ejp@eigence.com>
2019-11-05 08:33:02 -0500

    parallelize, sort out tokens to use

--- get_order_splitting_data.py
+++ get_order_splitting_data.py
@@ -1,7 +1,8 @@
 import json
 import sys
 from collections import defaultdict
-from datetime import datetime
+import concurrent.futures
+
 import v2_client
 import dexag_client
 import oneinch_client
@@ -14,24 +15,35 @@ TOTLE_EX = v2_client.name()
 
 QUOTE = 'ETH'
 
-# Tokens supported by Totle and most aggs
-TOKENS = ['ABYSS','ANT','AST','BAT','BNT','CDAI','CDT','CETH','CND','CUSDC','CVC','CWBTC','CZRX','DAI','DENT','ENG','ENJ','ETHOS','FUN','GNO','KNC','LEND','LINK','LRC','MANA','MCO','MKR','MLN','MTL','NEXO','NPXS','OMG','PAX','PAY','PLR','POE','POLY','POWR','RCN','RDN','REN','REP','REQ','RLC','RPL','SNT','SNX','SPANK','STORJ','TAU','TKN','TUSD','USDC','USDT','VERI','WBTC','XDCE','ZRX']
+# # Tokens not priced by any aggs, including Totle
+# UNSUPPORTED_TOKENS = ['CBAT','CREP','IETH','IKNC','ILINK','IREP','IUSDC','IWBTC','IZRX','PPP','PPT','SETH','WTC']
+# # Tokens priced by one or more aggs, but not Totle
+# MORE_AGG_TOKENS = ['ABT','APPC','BLZ','BTU','CBI','DAT','DGX','DTA','ELF','EQUAD','GEN','IDAI','LBA','MOC','MYB','OST','QKC','SPN','UPP','WETH','XCHF']
 
-# Tokens not supported by any aggs, including Totle
-UNSUPPORTED_TOKENS = ['CBAT','CREP','IETH','IKNC','ILINK','IREP','IUSDC','IWBTC','IZRX','PPP','PPT','SETH','WTC']
+# 38 Tokens priced by all aggs (maybe Totle)
+ALL_AGGS_TOKENS = ['ABT','ANT','BAT','BNT','BTU','CVC','DAI','DGX','ELF','ENJ','GEN','GNO','KNC','LINK','MANA','MKR','MLN','MTL','OMG','PAX','POE','POLY','POWR','RCN','RDN','REN','REP','REQ','RLC','SNT','SNX','TKN','TUSD','USDC','USDT','WBTC','WETH','ZRX']
+# Tokens priced by 1-Inch and DEX.AG (maybe Totle)
+MORE_ONEINCH_DEXAG_TOKENS = ['ABYSS','APPC','AST','BLZ','CBI','CDT','CND','ENG','EQUAD','ETHOS','FUN','LBA','LEND','LRC','MCO','MOC','NEXO','OST','PAY','PLR','QKC','RPL','SPANK','SPN','STORJ','TAU','UPP','XCHF','XDCE']
+# Tokens only priced by 1-Inch (maybe Totle)
+MORE_ONEINCH_TOKENS = ['DAT','DENT','DTA','MYB','NPXS']
+# Tokens only priced by DEX.AG (maybe Totle)
+MORE_DEX_AG_TOKENS = ['CDAI','CETH','CUSDC','CWBTC','CZRX','IDAI','VERI']
+# Tokens only priced by Paraswap (maybe Totle)
+MORE_PARASwAP_TOKENS = []
+# 47 Tokens priced by Totle and all aggs
+TOTLE_ONEINCH_DEXAG_TOKENS = ['ANT','AST','BAT','BNT','CDT','CND','CVC','DAI','ENG','ENJ','ETHOS','FUN','GNO','KNC','LEND','LINK','MANA','MCO','MKR','MTL','NEXO','OMG','PAX','PAY','PLR','POE','POLY','POWR','RCN','RDN','REN','REP','REQ','RLC','RPL','SNT','SNX','SPANK','STORJ','TAU','TKN','TUSD','USDC','USDT','WBTC','XDCE','ZRX']
+# 3 Tokens not priced by Totle but were by both 1-Inch and DEX.AG (maybe Paraswap)
+TOTLE_UNPRICED_TOKENS_TO_TRY = ['ABYSS','LRC','MLN']
 
-# These are tokens supported by one or more aggs, but not Totle
-MORE_AGG_TOKENS = ['ABT','APPC','BLZ','BTU','CBI','DAT','DGX','DTA','ELF','EQUAD','GEN','IDAI','LBA','MOC','MYB','OST','QKC','SPN','UPP','WETH','XCHF']
 
-# These are tokens supported by Totle but no aggs
-MORE_TOTLE_TOKENS = ['BMC']
-
-TRADE_SIZES = [0.1, 0.5, 1.0, 5.0, 10.0, 50.0, 100.0, 200.0, 300.0, 400.0]
+print(len(TOTLE_ONEINCH_DEXAG_TOKENS))
+exit(0)
+TRADE_SIZES = [0.1, 0.5, 1.0, 5.0, 10.0, 50.0, 100.0, 200.0, 300.0, 400.0, 500.0]
 
 # don't bother recording price info for these DEXs; they are never going to be used in splits
 EXCLUDE_DEXS = ['ag', 'IDEX', 'DDEX', 'Ethfinex', 'Paradex']
 
-def get_agg_data(*clients, tokens=TOKENS, trade_sizes=TRADE_SIZES, quote=QUOTE):
+def get_agg_data(*agg_clients, tokens=ALL_AGGS_TOKENS, trade_sizes=TRADE_SIZES, quote=QUOTE):
     filename_base = get_filename_base(dir=DATA_DIR, prefix='agg')
 
     # get list of tokens on dexag and 1-inch that are tradable/splittable
@@ -44,10 +56,16 @@ def get_agg_data(*clients, tokens=TOKENS, trade_sizes=TRADE_SIZES, quote=QUOTE):
     for base in tokens:
         for trade_size in trade_sizes:
             print(f"Doing {base} at {trade_size} {quote} ...")
+            futures_agg = {}
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                for agg_client in agg_clients:
+                    future = executor.submit(agg_client.get_quote, quote, base, from_amount=trade_size, dex='all')
+                    futures_agg[future] = agg_client.name()
+
             dexs_with_pair, splits_by_agg, agg_prices, dex_prices = set(), {}, {}, {}
-            for client in clients:
-                pq = client.get_quote(quote, base, from_amount=trade_size, dex='all')
-                agg_name = client.name()
+            for f in concurrent.futures.as_completed(futures_agg):
+                agg_name = futures_agg[f]
+                pq = f.result()
                 if not pq:
                     print(f"{agg_name} did not quote {quote} to {base} at trade size={trade_size}")
                 else:
@@ -78,7 +96,7 @@ COMPOUND_TOKENS = ['CBAT','CDAI','CETH','CREP','CUSDC','CWBTC','CZRX']
 TOTLE_EXCHANGES = integrated_exchanges = list(v2_client.exchanges().keys())
 
 
-def get_totle_data(tokens=TOKENS, trade_sizes=TRADE_SIZES, quote=QUOTE, exchanges=TOTLE_EXCHANGES):
+def get_totle_data(tokens=ALL_AGGS_TOKENS, trade_sizes=TRADE_SIZES, quote=QUOTE, exchanges=TOTLE_EXCHANGES):
     filename_base = get_filename_base(dir=DATA_DIR, prefix='totle')
 
     # get list of tokens on dexag and 1-inch that are tradable/splittable
@@ -127,11 +145,13 @@ if len(sys.argv) < 2:
     print(f"usage: {sys.argv[0]} totle|aggs")
     exit(0)
 
+tokens_to_try = sorted(set(TOTLE_ONEINCH_DEXAG_TOKENS + TOTLE_UNPRICED_TOKENS_TO_TRY))
+
 if sys.argv[1] == 'totle':
-    get_totle_data(tokens=['BAT', 'DAI'], trade_sizes=[0.2])
-    get_totle_data(tokens=sorted(set(TOKENS + MORE_TOTLE_TOKENS)))
+    # get_totle_data(tokens=['BAT', 'DAI'], trade_sizes=[0.2])
+    get_totle_data(tokens=tokens_to_try)
 elif sys.argv[1] == 'aggs':
     # get_agg_data(dexag_client, oneinch_client, paraswap_client, tokens=['BAT', 'DAI'], trade_sizes=[0.2])
-    get_agg_data(dexag_client, oneinch_client, paraswap_client, tokens=sorted(set(TOKENS + MORE_AGG_TOKENS)))
+    get_agg_data(dexag_client, oneinch_client, paraswap_client, tokens=tokens_to_try)
 else:
     print(f"Unrecognized data set '{sys.argv[1]}'")
