3494c9b686db53a511d6f338ab425661a17980aa
Ed Posnak <ejp@eigence.com>
2019-11-20 12:51:07 -0800

    extract main

--- totle_vs_aggs.py
+++ totle_vs_aggs.py
@@ -9,7 +9,7 @@ import paraswap_client
 import v2_client
 from v2_compare_prices import savings_data, print_savings, get_filename_base, SavingsCSV
 
-def compare_totle_and_aggs(agg_clients, base, quote, trade_size):
+def compare_totle_and_aggs(agg_clients, base, quote, trade_size, order_type='buy'):
     agg_savings = {}
     from_token, to_token = quote, base
     # this hardcodes sourceAmount=trade_size because from_token (AKA quote) is ETH
@@ -35,8 +35,6 @@ def compare_totle_and_aggs(agg_clients, base, quote, trade_size):
                 splits = exchange_utils.canonical_keys(pq['exchanges_parts'])
                 savings = savings_data(order_type, trade_size, base, agg_name, pct_savings, totle_used, totle_price, agg_price, splits)
                 agg_savings[agg_name] = savings
-                csv_writer.append(savings)
-
                 trade_info = f"trade size={trade_size} ETH (Totle price={totle_price:.5g} {agg_name} price={agg_price:.5g})"
                 if splits: trade_info += f"splits={splits}"
                 print(f"Totle saved {pct_savings:.2f} percent vs {agg_name} {order_type}ing {base} on {','.join(totle_used)} {trade_info}")
@@ -46,34 +44,36 @@ def compare_totle_and_aggs(agg_clients, base, quote, trade_size):
     return agg_savings
 
 ########################################################################################################################
-# main
-
-working_dir = os.path.dirname(__file__)
-if working_dir: os.chdir(working_dir)
-
-quote='ETH'
-order_type = 'buy'
-
-TOTLE_39 = ['ANT','AST','BAT','BNT','CDT','CND','CVC','DAI','ENG','ENJ','ETHOS','GNO','KNC','LINK','MANA','MCO','MKR','OMG','PAX','PAY','POE','POLY','POWR','RCN','RDN','REN','REP','REQ','RLC','RPL','SNT','SNX','STORJ','TKN','TUSD','USDC','USDT','WBTC','ZRX']
-AGG_CLIENTS = [dexag_client, oneinch_client, paraswap_client]
-all_buy_savings = defaultdict(lambda: defaultdict(lambda: defaultdict(dict))) # extra lambda prevents KeyError in print_savings
-
-TOKENS, TRADE_SIZES  = TOTLE_39, [0.1, 0.5, 1.0, 5.0, 10.0, 50.0, 100.0, 200.0, 300.0, 400.0, 500.0]
-# TOKENS, TRADE_SIZES = ['CVC', 'DAI', 'LINK'], [0.5, 5.0]
-
-filename = get_filename_base(prefix='totle_vs_aggs', suffix=order_type)
+def main():
 
 
-with SavingsCSV(filename) as csv_writer:
-    for base in TOKENS:
-        for trade_size in TRADE_SIZES:
-            agg_savings = compare_totle_and_aggs(AGG_CLIENTS, base, quote, trade_size)
-            for agg_name in agg_savings:
-                all_buy_savings[agg_name][base][trade_size] = agg_savings[agg_name]
+    working_dir = os.path.dirname(__file__)
+    if working_dir: os.chdir(working_dir)
 
-# print(json.dumps(all_buy_savings, indent=3))
+    quote='ETH'
+    order_type = 'buy'
 
-# Prints a savings dict, token => trade_size => savings values
-for agg_name in all_buy_savings:
-    print_savings(order_type, all_buy_savings[agg_name], TRADE_SIZES, title=f"Savings vs. {agg_name}")
+    TOTLE_39 = ['ANT','AST','BAT','BNT','CDT','CND','CVC','DAI','ENG','ENJ','ETHOS','GNO','KNC','LINK','MANA','MCO','MKR','OMG','PAX','PAY','POE','POLY','POWR','RCN','RDN','REN','REP','REQ','RLC','RPL','SNT','SNX','STORJ','TKN','TUSD','USDC','USDT','WBTC','ZRX']
+    AGG_CLIENTS = [dexag_client, oneinch_client, paraswap_client]
+    all_buy_savings = defaultdict(lambda: defaultdict(lambda: defaultdict(dict))) # extra lambda prevents KeyError in print_savings
+
+    TOKENS, TRADE_SIZES  = TOTLE_39, [0.1, 0.5, 1.0, 5.0, 10.0, 50.0, 100.0, 200.0, 300.0, 400.0, 500.0]
+    # TOKENS, TRADE_SIZES = ['CVC', 'DAI', 'LINK'], [0.5, 5.0]
+
+    filename = get_filename_base(prefix='totle_vs_aggs', suffix=order_type)
+
+
+    with SavingsCSV(filename) as csv_writer:
+        for base in TOKENS:
+            for trade_size in TRADE_SIZES:
+                agg_savings = compare_totle_and_aggs(AGG_CLIENTS, base, quote, trade_size)
+                for agg_name, savings in agg_savings.items():
+                    all_buy_savings[agg_name][base][trade_size] = savings
+                    csv_writer.append(savings)
+
+    # print(json.dumps(all_buy_savings, indent=3))
+
+    # Prints a savings dict, token => trade_size => savings values
+    for agg_name in all_buy_savings:
+        print_savings(order_type, all_buy_savings[agg_name], TRADE_SIZES, title=f"Savings vs. {agg_name}")
 
