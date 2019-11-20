d5a96a50b27d7b98c255e3f208bd7b37fff23a9d
Ed Posnak <ejp@eigence.com>
2019-11-20 12:50:29 -0800

    test custom fieldnames

--- test/test_v2_compare_prices.py
+++ test/test_v2_compare_prices.py
@@ -1,5 +1,8 @@
 import v2_compare_prices
 
+# with v2_compare_prices.SavingsCSV('foobar', fieldnames="foo bar".split()) as csv_writer:
+#     csv_writer.append({'foo':1, 'bar':2})
+
 avg_savings = {
     'BAT' : {
         'Uniswap': {'pct_savings': 0.02},
