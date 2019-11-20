73b14f56a8b5b32a5837885e914bcc7dbc879e31
Ed Posnak <ejp@eigence.com>
2019-11-20 12:49:53 -0800

    add debug

--- test/test_paraswap_client.py
+++ test/test_paraswap_client.py
@@ -5,13 +5,13 @@ dexs = paraswap_client.exchanges()
 for dex_name in dexs:
     can_name = exchange_utils.canonical_name(dex_name)
     print(f"{can_name} => {paraswap_client.DEX_NAME_MAP[can_name]}")
-exit(0)
 
 from_token, to_token = 'ETH', 'OMG'
-from_amount, to_amount = 1.0, 200.0
+from_amount, to_amount = 250.0, 200.0
 
-pq = paraswap_client.get_quote(from_token, to_token, from_amount=from_amount)
+pq = paraswap_client.get_quote(from_token, to_token, from_amount=from_amount, debug=True)
 print(f"price={pq['price']} from {from_token} to {to_token} from_amount={from_amount}")
+exit(0)
 
 try:
     pq = paraswap_client.get_quote(from_token, to_token, to_amount=to_amount)
