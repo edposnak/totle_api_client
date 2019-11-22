72476b218fe8c9b210c7ecee894a18e3bb65cee8
Ed Posnak <ejp@eigence.com>
2019-11-22 12:46:41 -0800

    collect and write ex_prices

--- totle_vs_aggs.py
+++ totle_vs_aggs.py
@@ -9,7 +9,6 @@ import paraswap_client
 import v2_client
 from v2_compare_prices import savings_data, get_pct_savings, print_savings, get_filename_base, SavingsCSV
 
-
 def compare_totle_and_aggs(agg_clients, base, quote, trade_size, order_type='buy'):
     agg_savings = {}
     from_token, to_token = quote, base
@@ -47,25 +46,26 @@ def compare_totle_and_aggs(agg_clients, base, quote, trade_size, order_type='buy
 
 ########################################################################################################################
 def main():
-
-
     working_dir = os.path.dirname(__file__)
     if working_dir: os.chdir(working_dir)
 
     quote='ETH'
     order_type = 'buy'
 
-    TOTLE_39 = ['ANT','AST','BAT','BNT','CDT','CND','CVC','DAI','ENG','ENJ','ETHOS','GNO','KNC','LINK','MANA','MCO','MKR','OMG','PAX','PAY','POE','POLY','POWR','RCN','RDN','REN','REP','REQ','RLC','RPL','SNT','SNX','STORJ','TKN','TUSD','USDC','USDT','WBTC','ZRX']
     AGG_CLIENTS = [dexag_client, oneinch_client, paraswap_client]
     all_buy_savings = defaultdict(lambda: defaultdict(lambda: defaultdict(dict))) # extra lambda prevents KeyError in print_savings
 
-    TOKENS, TRADE_SIZES  = TOTLE_39, [0.1, 0.5, 1.0, 5.0, 10.0, 50.0, 100.0, 200.0, 300.0, 400.0, 500.0]
+    TOTLE_39 = ['ANT','AST','BAT','BNT','CDT','CND','CVC','DAI','ENG','ENJ','ETHOS','GNO','KNC','LINK','MANA','MCO','MKR','OMG','PAX','PAY','POE','POLY','POWR','RCN','RDN','REN','REP','REQ','RLC','RPL','SNT','SNX','STORJ','TKN','TUSD','USDC','USDT','WBTC','ZRX']
+    HI_SPLIT_TOKENS = ['BAT', 'ENJ', 'GNO', 'KNC', 'MANA', 'OMG', 'POE', 'POWR', 'RCN', 'RDN', 'REN', 'REP', 'REQ', 'RLC', 'SNT']
+    TOKENS = TOTLE_39
+
+    TRADE_SIZES  = [0.1, 0.5, 1.0, 5.0, 10.0, 50.0, 100.0, 200.0, 300.0, 400.0, 500.0]
     # TOKENS, TRADE_SIZES = ['CVC', 'DAI', 'LINK'], [0.5, 5.0]
 
-    filename = get_filename_base(prefix='totle_vs_aggs', suffix=order_type)
+    CSV_FIELDS = "time action trade_size token exchange exchange_price totle_used totle_price pct_savings splits ex_prices".split()
 
-
-    with SavingsCSV(filename) as csv_writer:
+    filename = get_filename_base(prefix='totle_vs_agg_splits', suffix=order_type)
+    with SavingsCSV(filename, fieldnames=CSV_FIELDS) as csv_writer:
         for base in TOKENS:
             for trade_size in TRADE_SIZES:
                 agg_savings = compare_totle_and_aggs(AGG_CLIENTS, base, quote, trade_size)
@@ -79,3 +79,6 @@ def main():
     for agg_name in all_buy_savings:
         print_savings(order_type, all_buy_savings[agg_name], TRADE_SIZES, title=f"Savings vs. {agg_name}")
 
+
+if __name__ == "__main__":
+    main()
