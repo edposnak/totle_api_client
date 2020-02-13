1051bc83b1a6edd1c78acb06ad94885b98c4a2aa
Ed Posnak <ejp@eigence.com>
2020-02-13 13:44:12 -0800

    find candidate tokens by vol and market cap

--- test/test_oneinch_client.py
+++ test/test_oneinch_client.py
@@ -33,6 +33,8 @@ def test_get_quote(to_token, from_token='ETH', from_amount=None, to_amount=None,
     except (oneinch_client.DexAGAPIException, ValueError) as e:
         print(e)
 
+#######################################################################################################################
+
 test_dex_name_map()
 exit(0)
 
