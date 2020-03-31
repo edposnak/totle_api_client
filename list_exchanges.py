a99ee642c0b776e4c6dfbce68c76a952f2889bd2
Ed Posnak <ejp@eigence.com>
2020-03-30 22:32:23 -0700

    print more exchanges and ids

--- list_exchanges.py
+++ list_exchanges.py
@@ -2,3 +2,11 @@
 
 import totle_client
 print(totle_client.enabled_exchanges())
+
+print(f"\nSwap Exchanges")
+for name, id in totle_client.exchanges().items():
+    print(f"{id:<3}: {name}")
+
+print(f"\nData Exchanges")
+for name, id in totle_client.data_exchanges().items():
+    print(f"{id:<3}: {name}")
